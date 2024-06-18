2c7b8f113de3dfc59174ec2783687e2e4e9b2a29
Ralf Böhler <ralf.boehler.90@gmail.com>
2024-06-18 15:07:05 +0200

    fix: throw error when config file could not be found

--- src/config/models.rs
+++ src/config/models.rs
@@ -1,14 +1,14 @@
 use tracing::level_filters::LevelFilter;
 
-use std::sync::OnceLock;
 use config::{Config, File, FileFormat};
 use serde::Deserialize;
+use std::sync::OnceLock;
 
 #[derive(Deserialize, Debug, Clone)]
 pub struct ScriptConfig {
     dracoon: DracoonConfig,
     logging: LoggingConfig,
-    activate_virus_protection: Vec<u64>
+    activate_virus_protection: Vec<u64>,
 }
 
 #[derive(Deserialize, Debug, Clone)]
@@ -73,48 +73,30 @@ pub enum DebugLevel {
     /// Designates very serious errors.
     #[serde(rename = "error")]
     Error,
-
 }
-
 
 impl ScriptConfig {
     pub fn init(path: Option<String>) -> &'static ScriptConfig {
-        static CONFIG:  OnceLock<ScriptConfig> = OnceLock::new();
+        static CONFIG: OnceLock<ScriptConfig> = OnceLock::new();
         let relative_path = path.unwrap_or("./config.yml".to_string());
 
-        let path = process_path::get_executable_path();
-
-        let mut path = match path {
-            Some(p) => p,
-            None => {
-                println!("Failed to get executable path as a result using default config. Log level is to error. No log file is created.");
-                return CONFIG.get_or_init(|| ScriptConfig {
-                    logging: LoggingConfig {
-                        log_level: DebugLevel::Error,
-                        log_file: None,
-                    },
-                    activate_virus_protection: Vec::new(),
-                    dracoon: DracoonConfig {
-                        base_url: "https://dracoon.team".to_string(),
-                    }
-                });
-            }
-        };
-        
+        let mut path = process_path::get_executable_path().expect("Failed to get executable path.");
         path = path.parent().unwrap().to_path_buf();
         path.push(relative_path);
-        
+
         let config = CONFIG.get_or_init(|| {
             Config::builder()
-            .add_source(File::new(path.to_str().unwrap(), FileFormat::Yaml).required(true))
-            .build()
-            .map_err(|e| {
-                println!("Couldn't find a config file. {}", e);
-            }).unwrap()
-            .try_deserialize::<ScriptConfig>()
-            .map_err(|e| {
-                println!("Failed to deserialize config: {}", e);
-            }).unwrap()
+                .add_source(File::new(path.to_str().unwrap(), FileFormat::Yaml).required(true))
+                .build()
+                .map_err(|e| {
+                    println!("Couldn't find a config file. {}", e);
+                })
+                .unwrap()
+                .try_deserialize::<ScriptConfig>()
+                .map_err(|e| {
+                    println!("Failed to deserialize config: {}", e);
+                })
+                .unwrap()
         });
         config
     }
